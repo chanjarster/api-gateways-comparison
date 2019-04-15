01aac87a3f74a384bd935e9105a0b2b048404b69
Daniel Qian <chanjarster@gmail.com>
2019-04-15 11:30:49 +0800

    change vertx code

--- vertx/src/main/java/me/chanjar/api_gateway/vertx/Proxy.java
+++ vertx/src/main/java/me/chanjar/api_gateway/vertx/Proxy.java
@@ -1,6 +1,7 @@
 package me.chanjar.api_gateway.vertx;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpClient;
@@ -19,7 +20,9 @@ public class Proxy extends AbstractVerticle {
   // Convenience method so you can run it in your IDE
   public static void main(String[] args) {
     Vertx vertx = Vertx.vertx(new VertxOptions().setPreferNativeTransport(true));
-    vertx.deployVerticle(Proxy.class.getName());
+    DeploymentOptions deploymentOptions = new DeploymentOptions()
+        .setInstances(Runtime.getRuntime().availableProcessors());
+    vertx.deployVerticle(Proxy.class.getName(), deploymentOptions);
   }
 
   @Override
